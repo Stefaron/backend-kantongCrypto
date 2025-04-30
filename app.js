971df0fc82ad622c702d7dbd1cc11db6ede2053c
Stefaron Budhi Santosa <93313500+Stefaron@users.noreply.github.com>
2025-04-30 20:36:30 +0700

    add cors

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ import express from "express";
 import walletRoutes from "./routes/walletRoutes.js";
 import transactionRoutes from "./routes/transactionRoutes.js";
 import setupSwagger from "./config/swagger.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 // require("dotenv").config();
 
 app.use(express.json());
